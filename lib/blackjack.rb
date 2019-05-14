8eb39d10550173c176bf427e2ad826493bc7d209
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-14 23:53:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,5 +46,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game(card_total)
 end
